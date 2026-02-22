788dcf442c864799c8a09e37d7354019bafb17e9
IT24102336 <IT24102336@my.sliit.lk>
2026-02-22 22:37:47 +0530

    bloodtestresult.java

--- lifeline-backend/src/main/java/com/lifeline/model/BloodTestResult.java
+++ lifeline-backend/src/main/java/com/lifeline/model/BloodTestResult.java
@@ -6,10 +6,5 @@ import lombok.Data;
 public class BloodTestResult {
     private String bloodBagId; // Or inventory ID
     private boolean hivPositive;
-    private boolean hepatitisPositive;
-    private boolean malariaPositive;
-    private boolean syphilisPositive;
-    
-    // Safety Validation Engine Logic:
-    // IF any == true -> Bio-Hazard
+   
 }
